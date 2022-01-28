91f225f1c72606b4204ea989f3af3c6439b3c2b6
Milo Fultz <milopfultz@gmail.com>
2022-01-28 13:54:21 -0800

    Update timer colors

--- timer.c
+++ timer.c
@@ -10,21 +10,30 @@ int main( int argc, char *argv[] )
     int seconds = val % 60;
 
     initscr();
+    start_color();
+    curs_set(0);
+
     attron(A_BOLD);
-    printw("%s\n\n", task);
+    move(1, 2);
+    printw("%s", task);
     attroff(A_BOLD);
+    move(5, 2);
+    attron(A_BOLD);
+    init_pair(1, COLOR_BLACK, COLOR_BLACK);
+    attron(COLOR_PAIR(1));
+    addstr("Use Ctrl + C to exit early");
+    attroff(COLOR_PAIR(1) | A_BOLD);
     refresh();
 
     while (val > 0)
     {
-        move(2, 0);
-        printw("%02d:%02d", minutes, seconds);
+        move(3, 2);
+        printw("%02d : %02d", minutes, seconds);
         refresh();
         sleep(1);
         val--;
         minutes = val / 60;
         seconds = val % 60;
-        move(2, 0);
     }
 
     val = 5;
